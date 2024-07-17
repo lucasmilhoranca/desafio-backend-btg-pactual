911830d31b2f82c284cd132a8aa41c03b4ee8566
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-07-16 21:55:31 -0300

    feat: adicionando testes a classe OrderService

--- src/test/java/tech/buildrun/btgpactual/orderms/factory/OrderEntityFactory.java
+++ src/test/java/tech/buildrun/btgpactual/orderms/factory/OrderEntityFactory.java
@@ -1,5 +1,7 @@
 package tech.buildrun.btgpactual.orderms.factory;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import tech.buildrun.btgpactual.orderms.entity.OrderEntity;
 import tech.buildrun.btgpactual.orderms.entity.OrderItem;
 
@@ -18,5 +20,9 @@ public class OrderEntityFactory {
         entity.setItems(List.of(items));
 
         return entity;
+    }
+
+    public static Page<OrderEntity> buildWithPage() {
+        return new PageImpl<>(List.of(build()));
     }
 }
